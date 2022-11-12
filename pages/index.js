0244b1a6fed36f6a108875c248e33066edea9d4b
manjunatha2 <manjunath.tantry@investwithtribe.com>
2022-11-12 17:24:29 +0530

    Fixes

--- pages/index.js
+++ pages/index.js
@@ -97,6 +97,9 @@ export default function Home() {
               </div>
             </div>
           </div>
+
+          <div>Share This Link</div>
+          <div></div>
         </div>
       </main>
     </div>
